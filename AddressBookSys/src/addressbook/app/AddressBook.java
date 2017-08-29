55634a2c00c5df0452ddfc1d91a7f0175a7f62d9
Keenan St-Denis <keenan.st-denis@dawsoncollege.qc.ca>
2017-08-29 15:18:21 -0400

    Create AddressBook application

---
+++ AddressBookSys/src/addressbook/app/AddressBook.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package addressbook.app;
+import lib.Address;
+
+/**
+ * @author 1636457
+ *
+ */
+public class AddressBook {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+	}
+
+}
